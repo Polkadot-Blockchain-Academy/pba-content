277e48d90b67c4babb765b1f5e42a77df13c16da
Dan Shields <nukemandan@protonmail.com>
2022-07-19 10:51:32 +0100

    add joshy code updates

--- syllabus/3-Blockchain/3-Blockchain-From-Scratch/src/p1_state_machine/p1_switches.rs
+++ syllabus/3-Blockchain/3-Blockchain-From-Scratch/src/p1_state_machine/p1_switches.rs
@@ -1,5 +1,6 @@
+
 //! We begin our hands on exploration of state machines with two very simple examples.
-//! In these examples, we use actually switch boards as the state machine. The state is,
+//! In these examples, we use actualy switch boards as the state machine. The state is,
 //! well, just the state of the switches.
 
 use super::StateMachine;
@@ -11,6 +12,7 @@ pub struct LightSwitch;
 /// We model this simple system as a state machine with a single transition - toggling the switch
 /// Because there is only a single kind of transition, we can use a unit struct.
 impl StateMachine for LightSwitch {
+
     type State = bool;
     type Transition = ();
 
@@ -24,20 +26,21 @@ impl StateMachine for LightSwitch {
 pub struct WeirdSwitchMachine;
 
 /// The state is now two switches instead of one so we use a struct.
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(PartialEq, Eq, Debug)]
 pub struct TwoSwitches {
-	first_switch: bool,
-	second_switch: bool,
+    first_switch: bool,
+    second_switch: bool,
 }
 
 /// Now there are two switches so we need a proper type for the transition.
 pub enum Toggle {
-	FirstSwitch,
-	SecondSwitch,
+    FirstSwitch,
+    SecondSwitch,
 }
 
 /// We model this system as a state machine with two possible transitions
-impl StateMachine for TwoSwitches {
+impl StateMachine for WeirdSwitchMachine {
+
     type State = TwoSwitches;
     type Transition = Toggle;
 
@@ -46,21 +49,22 @@ impl StateMachine for TwoSwitches {
     }
 }
 
+
 #[test]
 fn sm_1_light_switch_toggles_off() {
-	assert!(!LightSwitch::next_state(&true, &()));
+    assert!(!LightSwitch::next_state(&true, &()));
 }
 
 #[test]
 fn sm_1_light_switch_toggles_on() {
-	assert!(LightSwitch::next_state(&false, &()));
+    assert!(LightSwitch::next_state(&false, &()));
 }
 
 #[test]
 fn sm_1_two_switches_first_goes_on() {
-	let state = TwoSwitches { first_switch: false, second_switch: false };
+    let state = TwoSwitches { first_switch: false, second_switch: false };
 
-	assert_eq!(TwoSwitches::next_state(&state, &Toggle::FirstSwitch), TwoSwitches {
+    assert_eq!(WeirdSwitchMachine::next_state(&state, &Toggle::FirstSwitch), TwoSwitches {
         first_switch: true,
         second_switch: false,
     });
@@ -68,10 +72,10 @@ fn sm_1_two_switches_first_goes_on() {
 
 #[test]
 fn sm_1_two_switches_first_goes_off_second_was_on() {
-	// This is the special case. We have to make sure the second one goes off with it.
+    // This is the special case. We have to make sure the second one goes off with it.
     let state = TwoSwitches { first_switch: true, second_switch: true };
 
-	assert_eq!(TwoSwitches::next_state(&state, &Toggle::FirstSwitch), TwoSwitches {
+    assert_eq!(WeirdSwitchMachine::next_state(&state, &Toggle::FirstSwitch), TwoSwitches {
         first_switch: false,
         second_switch: false,
     });
@@ -79,10 +83,10 @@ fn sm_1_two_switches_first_goes_off_second_was_on() {
 
 #[test]
 fn sm_1_two_switches_first_goes_off_second_was_off() {
-	// This is adjascent to the special case. We have to make sure the second one stays off.
+    // This is adjascent to the special case. We have to make sure the second one stays off.
         let state = TwoSwitches { first_switch: true, second_switch: false };
 
-        assert_eq!(TwoSwitches::next_state(&state, &Toggle::FirstSwitch), TwoSwitches {
+        assert_eq!(WeirdSwitchMachine::next_state(&state, &Toggle::FirstSwitch), TwoSwitches {
             first_switch: false,
             second_switch: false,
         });
@@ -92,7 +96,7 @@ fn sm_1_two_switches_first_goes_off_second_was_off() {
 fn sm_1_two_switches_second_goes_on() {
     let state = TwoSwitches { first_switch: false, second_switch: false };
 
-    assert_eq!(TwoSwitches::next_state(&state, &Toggle::SecondSwitch), TwoSwitches {
+    assert_eq!(WeirdSwitchMachine::next_state(&state, &Toggle::SecondSwitch), TwoSwitches {
         first_switch: false,
         second_switch: true,
     });
@@ -102,7 +106,7 @@ fn sm_1_two_switches_second_goes_on() {
 fn sm_1_two_switches_second_goes_off() {
     let state = TwoSwitches { first_switch: true, second_switch: true };
 
-    assert_eq!(TwoSwitches::next_state(&state, &Toggle::SecondSwitch), TwoSwitches {
+    assert_eq!(WeirdSwitchMachine::next_state(&state, &Toggle::SecondSwitch), TwoSwitches {
         first_switch: true,
         second_switch: false,
     });
--- syllabus/3-Blockchain/3-Blockchain-From-Scratch/src/p1_state_machine/p3_atm.rs
+++ syllabus/3-Blockchain/3-Blockchain-From-Scratch/src/p1_state_machine/p3_atm.rs
@@ -23,24 +23,29 @@ pub enum Action {
     PressKey(Key),
 }
 
-/// The ATM itself. When you swipe your card, the ATM learns your correct pin.
+/// The ATM. When a card is swiped, the ATM learns the correct pin's hash.
 /// It waits for you to key in your pin. You can press as many numeric keys as
 /// you like followed by enter. If the pin is incorrect, your card is returned
 /// and the ATM automatically goes back to the main menu. If your pin is correct,
 /// the ATM waits for you to key in an amount of money to withdraw. Withdraws
 /// are bounded only by the cash in the machine (there is no account balance).
 pub struct Atm {
+    /// How much money is in the ATM
     cash_inside: u64,
-    expected_pin: Option<u64>,
-    pin_register: Vec<Key>,
+    /// The hash of a user's expected pin. The ATM will not expect any particular pin
+    /// before the user swipes their card.
+    expected_pin_hash: Option<u64>,
+    /// All the keys that have been pressed since the last `Enter`
+    keystroke_register: Vec<Key>,
 }
 
 impl StateMachine for Atm {
 
+    // Exercise: Fill in these associated types.
     type State = ();
     type Transition = ();
 
-    fn next_state(starting_state: &(), t: &()) -> () {
+    fn next_state(starting_state: &Self::State, t: &Self::Transition) -> () {
         todo!()
     }
 
