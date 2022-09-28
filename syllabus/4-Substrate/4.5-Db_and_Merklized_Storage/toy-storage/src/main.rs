94ce1a0e51a2b3e58183f69811124ab594a1464e
Joshy Orndorff <admin@joshyorndorff.com>
2022-09-28 09:49:55 -0400

    slide 20

--- syllabus/4-Substrate/4.5-Db_and_Merklized_Storage/toy-storage/src/main.rs
+++ syllabus/4-Substrate/4.5-Db_and_Merklized_Storage/toy-storage/src/main.rs
@@ -1,3 +1,24 @@
+use std::containers::HashMap;
+
 fn main() {
+	// This represents our Key-Value Database. In Substrate this would
+	// be RocksDB or ParityDB.
+	let mut kvdb = HashMap::new();
+
 	println!("Hello, world!");
+}
+
+pub enum NodeType {
+	Empty,
+	Leaf,
+	BranchNoValue,
+	BranchWithValue,
+}
+
+pub struct Node {
+	header: (),
+	key: (),
+	children: (), // I want to make this simple so we will use binary trees
+	// maybe I should have two fields leftChild and rightChild?
+	value: (),
 }
