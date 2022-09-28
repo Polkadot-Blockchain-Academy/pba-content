861f77a46c2b065a9966afe2a2e711604e810668
Joshy Orndorff <admin@joshyorndorff.com>
2022-09-28 16:04:29 -0400

    sketch some method signatures. impls will be exercises.

--- syllabus/4-Substrate/4.5-Db_and_Merklized_Storage/toy-storage/src/main.rs
+++ syllabus/4-Substrate/4.5-Db_and_Merklized_Storage/toy-storage/src/main.rs
@@ -10,10 +10,12 @@ fn main() {
 
 	// This represents our Key-Value Database. In Substrate this would
 	// be RocksDB or ParityDB. The key type is the hash of the Node itself.
-	let mut kvdb = HashMap::<u64, Node<String>>::new();
+	let mut kvdb = ::new();
+	let storage = StorageInstance()
 
 	println!("Hello, world!");
 }
+
 
 /// The key type is arbitrary data modelled as a simple bitstring Vec<bool>
 /// This choice also allows the use of a binary tree.
@@ -37,3 +39,33 @@ pub enum Node<Value> {
 		value: Value,
 	},
 }
+
+/// Represents a complete storage implemented using the Particia Merkle trie strategy.
+/// Underlying KeyValue storage is a simple hashmap.
+pub type StorageInstance<Value>{
+	kvdb: HashMap::<u64, Node<Value>>,
+	//TODO I need to know the root node _somehow_. One option is to track
+	// it explicitly like this. Another is to store it in the kvdb at a fixed key.
+	// I guess the empty vec would would be an appropriate key. I'll do that actually.
+	root_node: Node<Value>,
+}
+
+impl StorageInstance<Value> {
+	//! Check whether a particular key is in the trie and if it is, return its value.
+	pub fn get(Vec<bool>) -> Option<Value> {
+		todo!("Exercise")
+	}
+
+	//! Check whether the 
+	pub fn is_empty() -> bool {
+		todo!("exercise")
+	}
+
+	pub fn root() -> Node<Value> {
+		todo!("exercise")
+	}
+}
+
+
+// Dumb Question: Why are we building this whole logical trie instead
+// of just storing data in the kvdb directly?
